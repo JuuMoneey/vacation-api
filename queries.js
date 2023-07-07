082403fcc56198bead3acf05a5a5efa5f866b563
mattyllier <bloodrootfox@gmail.com>
2023-07-07 13:47:49 -0500

    updated pool properties

--- queries.js
+++ queries.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'travelapp',
+    database: 'travelApp',
     password: 'postgres',
     port: 5432,
 });
